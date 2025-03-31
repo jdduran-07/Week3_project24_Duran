5949e91a84a46fa16a4b453c652f93381904c611
jdduran-07 <jdduran@amarillocollege.com>
2025-03-31 15:35:34 -0500

    Add files via upload

---
+++ project02-04.js
@@ -0,0 +1,36 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-04
+
+      Application to calculate the cost of a restaurant order plus tax
+      Author: Joe Duran
+      Date:   03/31/2025
+
+      Filename: project02-04.js
+ */
+
+const CHICKEN_PRICE=10.95;
+const HALIBUT_PRICE=13.95;
+const BURGER_PRICE=9.95;
+const SALMON_PRICE=18.95;
+const SALAD_PRICE=7.95;
+const SALES_TAX = 0.07;
+
+cost =+ ("")
+
+
+calcTotal
+buyChicken = getElementByName.buyChicken ("clicked")
+buyHalibut = getElementByName.buyHalibut ("clicked")
+buyBurger = getElementByName.buyBurger ("clicked")
+buySalmon = getElementByName.buySalmon ("clicked")
+buySalad  = getElementByName.buySalad ("clicked")
+
+let [i] = 0 , 
+ 
+
+
+// Function to display a numeric value as a text string in the format $##.## 
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
